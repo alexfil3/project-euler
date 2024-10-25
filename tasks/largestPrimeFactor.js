ec668282a95d8e1b1f87bb3375976c45365bb39a
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-10-25 20:37:47 +0300

    solve third problem

--- tasks/largestPrimeFactor.js
+++ tasks/largestPrimeFactor.js
@@ -1,1 +1,22 @@
-function largestPrimeFactor() {}
+function largestPrimeFactor(n) {
+    let prime = 2;
+    let maxPrime = 1;
+
+    while (n % prime === 0) {
+        maxPrime = prime;
+        n /= prime; 
+    }
+
+    prime = 3;
+
+    while (n !== 1) {
+        while(n % prime === 0) {
+            maxPrime = prime;
+            n /= prime;
+        }
+
+        prime += 2;
+    }
+
+    return maxPrime;
+}
